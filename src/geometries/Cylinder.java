8f1ce7620d57b0ae622782443e7a8f4eef2d9919
achiya yunyan <ayunyan@g.jct.ac.il>
2022-03-21 18:58:36 +0200

    EX02 Finito

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -22,19 +22,22 @@ public class Cylinder extends Tube {
 
     @Override
     public Vector getNormal(Point point) {
-        double distance = point.distance(axisRay.getP0());
+        double distanceBase1 = point.distance(axisRay.getP0());
         Vector axisVec = axisRay.getDir();
 
         // the point is P0
-        if (Util.isZero(distance))
+        if (Util.isZero(distanceBase1))
             return axisRay.getDir().scale(-1).normalize();
-        Vector toP0 = point.subtract(axisRay.getP0());
+        Point centerBase2 = axisRay.getP0().add(axisVec.scale(height));
+        double distanceBase2 = centerBase2.distance(point);
         // point is on the center of the far side from P0
-        if (Util.isZero(distance - height) && Util.isZero(axisVec.dotProduct(toP0) - axisVec.length() * toP0.length()))
+        if (Util.isZero(distanceBase2))
             return axisVec.normalize();
-        else if (Util.isZero(axisVec.dotProduct(point.subtract(axisRay.getP0()))))
+        // point is on the close side to P0
+        else if (distanceBase1 < radius && distanceBase2 > distanceBase1)
             return axisVec.scale(-1).normalize();
-        else if (Util.isZero(axisVec.dotProduct(point.subtract(axisRay.getP0().add(axisVec.scale(height))))))
+        // point is on the far side from P0
+        else if (distanceBase2 < radius && distanceBase1 > distanceBase2)
             return axisVec.normalize();
         else {
             double t = axisRay.getDir().dotProduct(point.subtract(axisRay.getP0()));
