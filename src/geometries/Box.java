ae0b3cc1fe2066155323eef149717a145640929f
HarelDar <dar@yatmal.co.il>
2022-06-03 14:50:22 +0300

    Sphere & Cylinder

--- src/geometries/Box.java
+++ src/geometries/Box.java
@@ -6,15 +6,13 @@ import primitives.Ray;
 public class Box {
     Double3 p0;
     Double3 p1;
+
     public Box(Double3 p0, Double3 p1) {
         this.p0 = p0;
         this.p1 = p1;
     }
 
-
-    Boolean isIntersection(Ray ray){
+    Boolean isIntersection(Ray ray) {
         
     }
-    
-
 }
